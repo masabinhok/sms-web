418d9714692cc0b73074911bf407742c10dfe6b2
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2025-10-06 19:43:30 +0545

    Update homepage content and configure Next.js settings for server actions and environment variables

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,12 @@ export default function Home() {
   return (
    <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <div className="flex flex-col items-center">
-<h1 className="text-3xl uppercase font-bold">
+    <h1 className="text-3xl uppercase font-bold">
       ssms
     </h1>
-    <p>Your very own School Management System</p>
-    <button className="btn btn-primary mt-4">
-      Get Started
-    </button>
+    <p>
+      This is the homepage of the SMS application, where we provide free school website also managed by admin panel.
+    </p>
     </div>
     
    </main>
